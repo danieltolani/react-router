64a4b01526b75ad4b6459cdfde6af88c4d47256c
Tolani Daniel <mumytosin2016@gmail.com>
2022-11-01 06:33:47 +0100

    Dir: Homepage fix to direct to students' page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
     return(
         <section>
             <h1>Welcome to the Home page! </h1>
+            <p style={{marginTop: '0.6em'}}>Kindly Navigate to the Students' section 😎 </p>
         </section>
     )
 }
